0bd48b9c3275997ded368c3255f8bc2313ac3033
Shaheer Choudhury <shaheer.choudhury@gmail.com>
2019-09-03 20:34:56 -0400

    finished reinforcement

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
-function arrayToObj() {
-   // your code goes here  
+testarray = [['Alfonso Cuaron', 'Gravity'], ['Spike Jonze', 'Her'], ['Martin Scorsese', 'The Wolf of Wall Street']]
+
+function arrayToObj(array) {
+   let object = {};
+   array.forEach((e) => {
+      object[e[0]] = e[1];
+   });
+   return object;
 }
+
+console.log(arrayToObj(testarray));
 
 module.exports = arrayToObj;
